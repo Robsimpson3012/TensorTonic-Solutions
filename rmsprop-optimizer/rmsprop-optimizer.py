3168f356221994c1cddb03c73ffe4b0bafbcb2aa
Robsimpson3012 <59181837+Robsimpson3012@users.noreply.github.com>
2026-03-27 14:58:45 +0000

    Update solution for rmsprop-optimizer

--- rmsprop-optimizer/rmsprop-optimizer.py
+++ rmsprop-optimizer/rmsprop-optimizer.py
@@ -4,7 +4,7 @@ def rmsprop_step(w, g, s, lr=0.001, beta=0.9, eps=1e-8):
     """
     Perform one RMSProp update step.
     """
-    w =np.array(w)
+    w = np.array(w)
     g = np.array(g)
     s = np.array(s)
     
